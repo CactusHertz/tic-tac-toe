fa9bf3abf0c3c179e1268992498ba994f103f36d
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2024-02-18 15:31:53 -0800

    Add interaction of page with gameboard

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function gameboard () {
-    var board = [[1,2,3],[4,5,6],[7,8,9]];
-    var movesMade = 0;
+    let board = [[1,2,3],[4,5,6],[7,8,9]];
+    let movesMade = 0;
+    let piece = 'X';
 
     const getBoard = () => {
         return board;
     };
-  
-    const getMove = () => {
-        return movesMade;
-    };
 
-    const setPiece = (x, y, piece) => {
+    const setPiece = (x, y, square) => {
         if (validMove(x,y)){
             board[x][y] = piece;
+            square.textContent = piece;
             console.log(`valid move: ${piece} placed at ${x},${y}`);
             movesMade++;
+            togglePiece();
             return true;
         }
         else {
@@ -24,51 +23,94 @@ function gameboard () {
     };
 
     const validMove = (x, y) => {
-        if (board[x][y] != 'x' && board[x][y] != 'o'){     
+        if (board[x][y] != 'X' && board[x][y] != '0'){     
             return true;
         }
         return false;
     };
 
-    return {getBoard, getMove, setPiece};
-}
-
-function checkWin(tttboard, numMoves) {
-    // checks if the board is in a completed states
-
-    // check columns
-    for (let c = 0; c < 3; c++){
-        let piece = tttboard[c][0];
-        if (tttboard[c][1] == piece && tttboard[c][2] == piece){
+    const checkWin = () => {
+        // checks if the board is in a completed states
+    
+        // check columns
+        for (let c = 0; c < 3; c++){
+            let piece = board[c][0];
+            if (board[c][1] == piece && board[c][2] == piece){
+                return piece;
+            }
+        }
+        // checks rows
+        for (let r = 0; r < 3; r++){
+            let piece = board[0][r];
+            if (board[1][r] == piece && board[2][r] == piece){
+                return piece;
+            }
+        }
+        //check diagonals
+        let piece = board[0][0]
+        if(tttboard[1][1] == piece && board[2][2] == piece){
             return piece;
         }
-    }
-    // checks rows
-    for (let r = 0; r < 3; r++){
-        let piece = tttboard[0][r];
-        if (tttboard[1][r] == piece && tttboard[2][r] == piece){
+        piece = board[0][2]
+        if(board[1][1] == piece && board[2][0] == piece){
             return piece;
         }
-    }
-    //check diagonals
-    let piece = tttboard[0][0]
-    if(tttboard[1][1] == piece && tttboard[2][2] == piece){
-        return piece;
-    }
-    piece = tttboard[0][2]
-    if(tttboard[1][1] == piece && tttboard[2][0] == piece){
-        return piece;
-    }
-    // check full
-    if(numMoves >= 9){
-        return 'tie';
+        // check full
+        if(movesMade >= 9){
+            return 'tie';
+        }
+        return 'no'
     }
 
-    return 'no'
+    const togglePiece = () => {
+        if(piece == 'X'){
+            piece = 'O';
+        }
+        else if(piece == 'O'){
+            piece = 'X';
+        }
+    }
+
+    return {getBoard, checkWin, setPiece};
 }
+
+
 
 function gameController() {
     //controls the state of the game and gets user input 
+    let board = null;
+
+    const startGame = () => {
+        board = gameboard();
+    }
+
+    const boardSquares = document.querySelectorAll('.board-square');
+    boardSquares.forEach((div) => {
+        div.addEventListener('click',() =>{
+            if(board != null){
+                let r = div.getAttribute('r');
+                let c = div.getAttribute('c');
+
+                console.log(r + ' ' + c);            
+                board.setPiece(r, c, div);
+                boardDisplay(board.getBoard());
+            
+            }     
+            else{
+                console.log('no board');
+            }
+        });
+    });
+
+    const restartButton = document.querySelector('.restart');
+    restartButton.addEventListener('click', () => {
+        startGame();
+        boardSquares.forEach((div) => {
+            div.textContent = '';
+        });
+    });
+
+   return {startGame};
 }
 
 function boardDisplay (board) {
@@ -78,23 +120,5 @@ function boardDisplay (board) {
     }
 }
 
-
-
-const board = gameboard();
-boardDisplay(board.getBoard());
-console.log(board.getMove());
-console.log(checkWin(board.getBoard(), board.getMove()));
-
-board.setPiece(0, 0, 'x');
-boardDisplay(board.getBoard());
-console.log(checkWin(board.getBoard(), board.getMove()));
-
-board.setPiece(0,1, 'x');
-boardDisplay(board.getBoard());
-console.log(checkWin(board.getBoard(), board.getMove()));
-
-board.setPiece(0,2, 'x');
-boardDisplay(board.getBoard());
-console.log(checkWin(board.getBoard(), board.getMove()));
-
-
+const gc = gameController();
+gc.startGame();
